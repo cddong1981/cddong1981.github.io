dfdd711f50a3cad56f03a1de9aae44a47796bc48
Chuanding Dong <dongchuanding@gmail.com>
2024-01-21 21:19:05 +0100

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example JavaScript for interactive elements
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page loaded!');
+
+    // Add more interactive JavaScript here as needed
+});
+
+
